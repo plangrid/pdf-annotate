c80d69a21ca87cfc2c0d9926634827a32b412520
Michael Bryant <michael.bryant@plangrid.com>
2018-08-01 14:13:46 -0700

    import metadata in package global

--- pdf_annotate/__init__.py
+++ pdf_annotate/__init__.py
@@ -2,6 +2,7 @@
 from pdf_annotate.annotator import PdfAnnotator
 from pdf_annotate.appearance import Appearance
 from pdf_annotate.location import Location
+from pdf_annotate.metadata import Metadata
 
 
 __all__ = ['PdfAnnotator']
