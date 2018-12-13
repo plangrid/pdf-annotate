efef87fae276b75d988211c32f6033b5cfc51328
Michael Bryant <michael.bryant@plangrid.com>
2018-12-13 11:00:34 -0800

    Include fonts file and try to exclude tests

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ setup(
     author='Michael Bryant',
     author_email='smart-recordset@plangrid.com',
     url='https://github.com/plangrid/pdf-annotate',
-    packages=find_packages('.'),
+    packages=find_packages('.', exclude=['tests*']),
+    include_package_data=True,
     install_requires=[
         'attrs>=18.1.0',  # this could probably be lower, but it's not tested
         'pdfrw>=0.4',
