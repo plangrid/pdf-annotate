8080efeabb6d187f65c48f3c016fa596801273fb
Michael Bryant <mbryantj@gmail.com>
2019-04-24 10:56:50 -0600

    Bump to v0.7.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='pdf-annotate',
-    version='0.6.4',
+    version='0.7.0',
     description='Add annotations to PDFs',
     author='Michael Bryant',
     author_email='smart-recordset@plangrid.com',
