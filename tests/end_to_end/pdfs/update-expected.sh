a296ec7a4a65c86a5f43476830be30596f76dceb
Michael Bryant <mbryantj@gmail.com>
2019-09-17 17:02:10 -0600

    Update expected results from end to end tests

---
+++ tests/end_to_end/pdfs/update-expected.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp end_to_end.pdf expected.pdf
+cp end_to_end_rotated_90.pdf expected_rotated_90.pdf
+cp end_to_end_rotated_180.pdf expected_rotated_180.pdf
+cp end_to_end_rotated_270.pdf expected_rotated_270.pdf
